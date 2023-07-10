f068e27b90558c37a1ed2d2991fc0dc8b5455d16
kimbeomjun <gimb52043@gmail.com>
2023-07-10 17:12:15 +0900

    tailwind css 연습

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="bg-slate-400 py-10 px-5 grid gap-5">
       <div className="bg-slate-50 p-10 rounded-xl shadow-xl">
         <span className="font-semibold text-lg">Select Item</span>
-        <div className="border-b pb-3 my-3">
+        <div className="border-b-2 border-dashed pb-3 my-3">
           <div className="flex justify-between">
             <span className="text-gray-500">Grey Chair</span>
             <span>$19</span>
@@ -15,11 +15,11 @@ export default function Home() {
             <span>$80</span>
           </div>
         </div>
-        <div className="flex justify-between">
+        <div className="flex justify-between mb-5">
           <span>Total</span>
           <span>$99</span>
         </div>
-        <div className="">checkout</div>
+        <div className="bg-blue-400 text-white p-3 text-center rounded-xl w-1/2 mx-auto">checkout</div>
       </div>
       <div className="bg-slate-50 p-10 rounded-xl shadow-xl"></div>
       <div className="bg-slate-50 p-10 rounded-xl shadow-xl"></div>
