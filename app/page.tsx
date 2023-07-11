1ad3206e3d1c1008bdb95fc0856d9687688e5e2a
kimbeomjun <gimb52043@gmail.com>
2023-07-11 15:06:50 +0900

    두번째 카드 스타일링

--- app/page.tsx
+++ app/page.tsx
@@ -19,9 +19,46 @@ export default function Home() {
           <span>Total</span>
           <span>$99</span>
         </div>
-        <div className="bg-blue-400 text-white p-3 text-center rounded-xl w-1/2 mx-auto">checkout</div>
+        <div className="bg-blue-400 text-white p-3 text-center rounded-xl w-1/2 mx-auto">
+          checkout
+        </div>
       </div>
-      <div className="bg-slate-50 p-10 rounded-xl shadow-xl"></div>
+      <div className="bg-slate-50 px-8 py-6 rounded-xl shadow-xl">
+        <div>
+          <div className="flex justify-between mb-3">
+            <div>⬅️</div>
+            <div className="space-x-3">
+              <span>4.9</span>
+              <span className="shadow-lg p-1 rounded-md">💖</span>
+            </div>
+          </div>
+          <div>
+            <img src="https://i.ibb.co/5963kz8/chair.jpg" alt="의자" />
+          </div>
+          <div className="mt-3">
+            <span className="block text-lg font-bold">Swoon Lounge</span>
+            <span className="text-sm text-slate-500">Chair</span>
+            <div>
+              <div className="flex justify-between items-center">
+                <div className="space-x-3">
+                  <input type="radio" className="accent-red-600" />
+                  <input type="radio" className="accent-blue-600" />
+                  <input type="radio" className="accent-orange-600" />
+                </div>
+                <div className="space-x-3">
+                  <button className="bg-slate-300 w-8 h-8 rounded-lg">-</button>
+                  <span >1</span>
+                  <button className="bg-slate-300 w-8 h-8 rounded-lg">+</button>
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-between items-center mt-6">
+              <span className="text-2xl font-medium">$450</span>
+              <button className="bg-blue-500 text-white px-10 py-3 rounded-lg">Add to cart</button>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="bg-slate-50 p-10 rounded-xl shadow-xl"></div>
       <div className="bg-slate-50 p-10 rounded-xl shadow-xl"></div>
     </div>
