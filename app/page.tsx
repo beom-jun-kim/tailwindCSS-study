260d198cb45a3ed3c25f2dbe8737533043871633
kimbeomjun <gimb52043@gmail.com>
2023-07-19 21:24:54 +0900

    responsive02

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,9 @@ const Button = tw.button`
 
 export default function Home() {
   return (
-    <div className="bg-slate-400 py-10 px-5 grid gap-5 text-black">
+
+    // place-content-center : 컨텐츠의 크기로 중앙 배치
+    <div className="xl:place-content-center bg-slate-400 py-10 px-5 grid gap-5 lg:grid-cols-2 xl:grid-cols-3 text-black">
       <div className="bg-slate-50 p-10 sm:bg-red-500 md:bg-teal-500 lg:bg-blue-500 xl:bg-yellow-400 2xl:bg-green-800 rounded-xl shadow-xl">
         <span className="font-semibold text-lg">Select Item</span>
         <div className="border-b-2 border-dashed pb-3 my-3 flex justify-between">
@@ -24,6 +26,46 @@ export default function Home() {
         </div>
       </div>
       <div className="bg-slate-50 px-8 py-6 rounded-xl shadow-xl group">
+        <div>
+          <div className="flex justify-between mb-3">
+            <div>⬅️</div>
+            <div className="space-x-3">
+              <span>4.9</span>
+              <span className="shadow-lg p-1 rounded-md">💖</span>
+            </div>
+          </div>
+          <div>
+            <img src="https://i.ibb.co/5963kz8/chair.jpg" alt="의자" />
+          </div>
+          <div className="mt-3">
+            <span className="block text-lg font-bold">Swoon Lounge</span>
+            <span className="text-sm text-slate-500">Chair</span>
+            <div>
+              <div className="flex justify-between items-center">
+                <div className="space-x-3">
+                  <button className="w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500 transition" />
+                  <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 transition" />
+                  <button className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2 ring-teal-500 transition" />
+                </div>
+                <div className="space-x-3">
+                  <Button>-</Button>
+                  <span>1</span>
+                  <Button>+</Button>
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-between items-center mt-6">
+              <span className="text-2xl font-medium">$450</span>
+
+              {/* group-hover */}
+              <button className="bg-blue-500 text-white px-10 py-3 rounded-lg group-hover:bg-slate-400 transition-colors">
+                Add to cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-slate-50 px-8 py-6 rounded-xl shadow-xl group lg:col-span-2 xl:col-span-1">
         <div>
           <div className="flex justify-between mb-3">
             <div>⬅️</div>
